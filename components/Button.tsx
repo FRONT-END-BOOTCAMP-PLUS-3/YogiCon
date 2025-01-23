924bbc3d4f5419ed897e17dcc7da0963ec06d8ad
JoowonJang <wndnjs5248@hanmail.net>
2025-01-23 16:35:27 +0900

    modify: Butotn 컴포넌트의 type prop을 isLong으로 변경

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,7 +6,7 @@ import styled from 'styled-components';
 const StyledButton = styled.button<ButtonProps>`
   width: 100%;
   min-width: 6.8125rem;
-  border-radius: ${({ type }) => (type === 'long' ? '0.9375rem' : '0.625rem')};
+  border-radius: ${({ isLong }) => (isLong ? '0.9375rem' : '0.625rem')};
   background-color: ${({ color }) => {
     switch (color) {
       case 'main':
@@ -19,14 +19,14 @@ const StyledButton = styled.button<ButtonProps>`
         return 'var(--disabled)';
     }
   }};
-  padding: ${({ type }) => (type === 'long' ? '0.5rem' : '0.625rem')};
+  padding: ${({ isLong }) => (isLong ? '0.5rem' : '0.625rem')};
   font-size: 1rem;
   font-weight: 700;
   color: ${({ color }) =>
     color === 'white' ? 'var(--black)' : 'var(--white)'};
   line-height: 1.25rem;
-  box-shadow: ${({ type }) =>
-    type === 'long' ? '0px 4px 4px 0px rgba(0, 0, 0, 0.25)' : ''};
+  box-shadow: ${({ isLong }) =>
+    isLong ? '0px 4px 4px 0px rgba(0, 0, 0, 0.25)' : ''};
   cursor: ${({ color }) => (color === 'disabled' ? 'not-allowed' : 'pointer')};
   border: ${({ color }) =>
     `0.0625rem solid ${color === 'white' ? 'var(--disabled)' : 'transparent'}`};
@@ -35,18 +35,18 @@ const StyledButton = styled.button<ButtonProps>`
 /* ---------------------------------- type ---------------------------------- */
 type ButtonProps = {
   children?: string;
-  type: 'long' | 'short';
+  isLong: boolean;
   color: 'main' | 'sub' | 'white' | 'disabled';
   onClick?: () => void;
 };
 
 /* -------------------------------- component ------------------------------- */
-const Button = ({ children, type, color, onClick }: ButtonProps) => {
+const Button = ({ children, isLong, color, onClick }: ButtonProps) => {
   const isDisabled = color === 'disabled';
 
   return (
     <StyledButton
-      type={type}
+      isLong={isLong}
       color={color}
       disabled={isDisabled}
       onClick={onClick}
