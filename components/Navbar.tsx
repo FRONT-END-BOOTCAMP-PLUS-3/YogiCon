b89573be863361744048354db66935fb6f12492c
sojeong <sojeong9432@gmail.com>
2025-01-23 12:30:02 +0900

    feat: Navbar 구현현

---
+++ components/Navbar.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { BiHome, BiMapAlt, BiUser } from 'react-icons/bi';
+import styled from 'styled-components';
+
+const NavbarBox = styled.div`
+  display: flex;
+  justify-content: space-around;
+  position: fixed;
+  bottom: 0px;
+  align-items: center;
+  width: 100%;
+  max-width: 768px;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 5.313rem;
+  border-top: 1px solid var(--disabled);
+`;
+
+const NavbarItem = styled(Link).withConfig({
+  shouldForwardProp: (prop) => prop !== 'isActive',
+})<NavbarItemProps>`
+  font-size: 2.5rem;
+  cursor: pointer;
+  color: ${(props) => (props.isActive ? `var(--main)` : `var(--sub)`)};
+`;
+
+type NavItems = {
+  name: string;
+  icon: React.ReactElement;
+  path: string;
+};
+
+type NavbarItemProps = {
+  isActive: boolean;
+};
+
+const Navbar: React.FC = () => {
+  const pathname = usePathname();
+  const navItems: NavItems[] = [
+    { name: 'Home', icon: <BiHome />, path: '/' },
+    { name: 'Map', icon: <BiMapAlt />, path: '/map' },
+    { name: 'MyInfo', icon: <BiUser />, path: '/mypage' },
+  ];
+
+  return (
+    <NavbarBox>
+      {navItems.map((item) => {
+        const isActive: boolean = pathname === item.path;
+        return (
+          <NavbarItem key={item.name} isActive={isActive} href={item.path}>
+            {item.icon}
+          </NavbarItem>
+        );
+      })}
+    </NavbarBox>
+  );
+};
+
+export default Navbar;
