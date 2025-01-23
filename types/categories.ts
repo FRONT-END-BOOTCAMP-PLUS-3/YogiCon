43ffbd7c138af5c5efbdebe56769985d3cec97db
yezzero <yeyeonggim06@gmail.com>
2025-01-23 15:54:50 +0900

    modify: 카테고리 string을 영어에서 한글로 변경

--- types/categories.ts
+++ types/categories.ts
@@ -1,9 +1,9 @@
 export type Categories =
-  | "cafe"
-  | "store"
-  | "giftcard"
-  | "icecream"
-  | "fastfood"
-  | "bakery"
-  | "beauty"
-  | "etc";
+  | "카페"
+  | "편의점"
+  | "상품권"
+  | "아이스크림"
+  | "패스트푸드"
+  | "베이커리"
+  | "뷰티"
+  | "기타";
