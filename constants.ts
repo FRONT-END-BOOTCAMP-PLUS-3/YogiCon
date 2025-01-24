5140e82d2a6854ad1327db0582ccad6fb66168fd
JoowonJang <wndnjs5248@hanmail.net>
2025-01-24 11:05:17 +0900

    feat: CATEGORY_LIST 상수 추가

---
+++ constants.ts
@@ -0,0 +1,13 @@
+import { CategoryListItem } from './types/categories';
+
+export const CATEGORY_LIST: CategoryListItem[] = [
+  '전체',
+  '카페',
+  '편의점',
+  '상품권',
+  '아이스크림',
+  '패스트푸드',
+  '베이커리',
+  '뷰티',
+  '기타',
+];
