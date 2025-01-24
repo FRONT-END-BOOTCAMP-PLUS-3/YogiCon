9733033727afed02ab6b071827c5126b5ffaa66b
JoowonJang <wndnjs5248@hanmail.net>
2025-01-24 11:45:07 +0900

    style: Button 컴포넌트의 styled-components props 이름 변경
    - $표시 붙이는 것으로 통일

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,13 +2,15 @@
 
 import styled from 'styled-components';
 
+type ButtonColor = 'main' | 'sub' | 'white' | 'disabled';
+
 /* ---------------------------------- style --------------------------------- */
-const StyledButton = styled.button<ButtonProps>`
+const StyledButton = styled.button<{ $isLong: boolean; $color: ButtonColor }>`
   width: 100%;
   min-width: 6.8125rem;
-  border-radius: ${({ isLong }) => (isLong ? '0.9375rem' : '0.625rem')};
-  background-color: ${({ color }) => {
-    switch (color) {
+  border-radius: ${({ $isLong }) => ($isLong ? '0.9375rem' : '0.625rem')};
+  background-color: ${({ $color }) => {
+    switch ($color) {
       case 'main':
         return 'var(--main)';
       case 'sub':
@@ -19,24 +21,26 @@ const StyledButton = styled.button<ButtonProps>`
         return 'var(--disabled)';
     }
   }};
-  padding: ${({ isLong }) => (isLong ? '0.5rem' : '0.625rem')};
+  padding: ${({ $isLong }) => ($isLong ? '0.5rem' : '0.625rem')};
   font-size: 1rem;
   font-weight: bold;
-  color: ${({ color }) =>
-    color === 'white' ? 'var(--black)' : 'var(--white)'};
+  color: ${({ $color }) =>
+    $color === 'white' ? 'var(--black)' : 'var(--white)'};
   line-height: 1.25rem;
-  box-shadow: ${({ isLong }) =>
-    isLong ? '0px 4px 4px 0px rgba(0, 0, 0, 0.25)' : ''};
-  cursor: ${({ color }) => (color === 'disabled' ? 'not-allowed' : 'pointer')};
-  border: ${({ color }) =>
-    `0.0625rem solid ${color === 'white' ? 'var(--disabled)' : 'transparent'}`};
+  box-shadow: ${({ $isLong }) =>
+    $isLong ? '0px 4px 4px 0px rgba(0, 0, 0, 0.25)' : ''};
+  cursor: ${({ $color }) =>
+    $color === 'disabled' ? 'not-allowed' : 'pointer'};
+  border: ${({ $color }) =>
+    `0.0625rem solid ${$color === 'white' ? 'var(--disabled)' : 'transparent'}`};
 `;
 
 /* ---------------------------------- type ---------------------------------- */
+
 type ButtonProps = {
   children?: string;
   isLong: boolean;
-  color: 'main' | 'sub' | 'white' | 'disabled';
+  color: ButtonColor;
   onClick?: () => void;
 };
 
@@ -46,8 +50,8 @@ const Button = ({ children, isLong, color, onClick }: ButtonProps) => {
 
   return (
     <StyledButton
-      isLong={isLong}
-      color={color}
+      $isLong={isLong}
+      $color={color}
       disabled={isDisabled}
       onClick={onClick}
     >
