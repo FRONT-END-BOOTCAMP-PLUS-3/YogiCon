f41882815256fa47465294fc07b353bf56e45d30
JoowonJang <wndnjs5248@hanmail.net>
2025-01-27 23:31:44 +0900

    feat: HomePage의 전체적인 스타일링
    - +(기프티콘 등록) 버튼 추가 예정

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,33 @@
 'use client';
 
-import { CategoryListItem } from '@/types/categories';
-import { useState } from 'react';
+import ConListItem from '@/components/ConListItem';
+import { Categories, CategoryListItem } from '@/types/categories';
+import { FormEvent, useState } from 'react';
+import styled from 'styled-components';
 import CategoryFilter from './components/CategoryFilter';
+import SearchForm from './components/SearchForm';
 
+/* ---------------------------------- style --------------------------------- */
+const HomeLayout = styled.div``;
+
+const FilterSection = styled.section`
+  position: sticky;
+  top: 1.5rem;
+  z-index: 9998;
+  background-color: var(--white);
+  border-bottom: 0.0625rem solid #e7e7e7;
+`;
+
+const ConListSection = styled.section`
+  padding-top: 1.9375rem;
+  padding-bottom: 5.375rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 1.9375rem;
+`;
+
+/* -------------------------------- page ------------------------------- */
 const Home = () => {
   const [selectedCategory, setSelectedCategory] =
     useState<CategoryListItem>('전체');
@@ -12,15 +36,97 @@ const Home = () => {
     setSelectedCategory(value);
   };
 
+  const handleSearch = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
-    <div style={{ overflow: 'hidden' }}>
-      {selectedCategory}
-      <CategoryFilter
-        selectedCategory={selectedCategory}
-        onSelect={handleCategoryButtonClick}
-      />
-    </div>
+    <HomeLayout>
+      <FilterSection aria-label="검색 및 필터링">
+        <SearchForm onSubmit={handleSearch} />
+        <CategoryFilter
+          selectedCategory={selectedCategory}
+          onSelect={handleCategoryButtonClick}
+        />
+      </FilterSection>
+      <ConListSection>
+        {dummyConList.map(
+          (item, index) =>
+            (selectedCategory === '전체' ||
+              selectedCategory === item.category) && (
+              <ConListItem
+                key={index}
+                category={item.category as Categories}
+                brand={item.brand}
+                name={item.name}
+                duedate={item.duedate}
+                isDeleted={item.isDeleted}
+              />
+            )
+        )}
+      </ConListSection>
+    </HomeLayout>
   );
 };
 
 export default Home;
+
+/* ------------------------------- dummy data ------------------------------- */
+const dummyConList = [
+  {
+    category: '패스트푸드',
+    brand: 'BHC',
+    name: '뿌링클',
+    duedate: '2025/01/28',
+    isDeleted: false,
+  },
+  {
+    category: '패스트푸드',
+    brand: 'BHC',
+    name: '뿌링클',
+    duedate: '2025/01/31',
+    isDeleted: false,
+  },
+  {
+    category: '패스트푸드',
+    brand: 'BHC',
+    name: '뿌링클',
+    duedate: '2025/02/03',
+    isDeleted: false,
+  },
+  {
+    category: '패스트푸드',
+    brand: 'BHC',
+    name: '뿌링클',
+    duedate: '2025/02/11',
+    isDeleted: false,
+  },
+  {
+    category: '패스트푸드',
+    brand: 'BHC',
+    name: '뿌링클',
+    duedate: '2025/02/13',
+    isDeleted: false,
+  },
+  {
+    category: '패스트푸드',
+    brand: 'BHC',
+    name: '뿌링클',
+    duedate: '2025/02/20',
+    isDeleted: false,
+  },
+  {
+    category: '패스트푸드',
+    brand: 'BHC',
+    name: '뿌링클',
+    duedate: '2025/02/25',
+    isDeleted: false,
+  },
+  {
+    category: '패스트푸드',
+    brand: 'BHC',
+    name: '뿌링클',
+    duedate: '2025/02/25',
+    isDeleted: false,
+  },
+];
