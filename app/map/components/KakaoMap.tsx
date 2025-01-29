6c8d988f80c4f0644822345f20bd5aa0e8dfddf8
yezzero <yeyeonggim06@gmail.com>
2025-01-29 17:39:51 +0900

    feat: 카카오맵 api로 지도 불러옴

---
+++ app/map/components/KakaoMap.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { useEffect } from 'react';
+import useGeolocation from '@/hooks/useGeolocation';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+const ReactKakaoMap = () => {
+  const apiKey: string | undefined = process.env.NEXT_PUBLIC_KAKAO_KEY;
+  const {location} = useGeolocation()
+
+  useEffect(() => {
+    if (!apiKey || !location) return;
+    
+    const script: HTMLScriptElement = document.createElement('script');
+    script.async = true;
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${apiKey}&autoload=false`;
+    document.head.appendChild(script);
+
+    script.addEventListener('load', () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById('map');
+        const coords = new window.kakao.maps.LatLng(
+          location?.latitude as number, 
+          location?.longitude as number
+        );
+
+        const options = {
+          center: coords,
+          level: 3,
+        };
+        const map = new window.kakao.maps.Map(container, options);
+
+        map.setCenter(coords);
+        console.log("셋했다:", coords)
+      });
+    });
+  }, [apiKey, location]);
+
+  return <div id="map" style={{ height: '100%', width: '100%' }} />;
+};
+
+export default ReactKakaoMap;
--- app/map/page.tsx
+++ app/map/page.tsx
@@ -2,25 +2,60 @@
 
 import { useState } from 'react';
 import styled from 'styled-components';
+import ReactKakaoMap from '@/app/map/components/KakaoMap';
+import { MdMyLocation } from 'react-icons/md';
 import BottomSheet from './components/BottomSheet';
 
+/* ---------------------------------- style --------------------------------- */
 const Wrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-color: rgb(252, 215, 255);
+  width: 100%;
+  height: calc(100vh - 30px);
+  overflow-y: hidden;
 `;
 
+const MapContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: var(--disabled);
+  position: relative;
+  z-index: 1;
+`;
+
+const MyLocation = styled.button`
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+  color: #777777;
+  width: 2.5rem;
+  aspect-ratio: 1/1;
+  border: none;
+  border-radius: 50%;
+  background-color: white;
+  box-shadow: 2px 2px 5px var(--disabled);
+  position: absolute;
+  left: 15px;
+  bottom: 160px;
+  z-index: 1;
+`;
+
+/* ---------------------------------- component --------------------------------- */
 export default function Map() {
   const [selectedItemKey, setSelectedItemKey] = useState<string | null>(null);
 
   return (
     <Wrapper>
-      <div style={{ paddingTop: '100px' }}></div>
-      <h1>선택된 아이템의 브랜드: {selectedItemKey ?? '없음'}</h1>
-      <BottomSheet
-        selectedItemKey={selectedItemKey}
-        setSelectedItemKey={setSelectedItemKey}
-      />
+      <MapContainer>
+        <ReactKakaoMap />
+        <MyLocation>
+          <MdMyLocation />
+        </MyLocation>
+        <BottomSheet
+          selectedItemKey={selectedItemKey}
+          setSelectedItemKey={setSelectedItemKey}
+        />
+      </MapContainer>
     </Wrapper>
   );
 }
