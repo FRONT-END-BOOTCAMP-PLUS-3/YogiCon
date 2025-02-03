44b24d984b295166c26037c3a5150152b35cf7ff
yezzero <yeyeonggim06@gmail.com>
2025-02-03 16:07:34 +0900

    modify: div를 li로 변경, props에 image_url 추가

--- app/map/components/Content.tsx
+++ app/map/components/Content.tsx
@@ -2,13 +2,21 @@
 
 import styled from 'styled-components';
 import ConListItem from '@/components/ConListItem';
-import { giftList } from './giftData';
+import { giftList } from '@/app/giftData';
 
 /* ---------------------------------- style --------------------------------- */
 const BSContentContainer = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+const BSContentList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 90%;
 `;
 
 /* ---------------------------------- type --------------------------------- */
@@ -30,19 +38,22 @@ export default function BottomSheetContent({
 
   return (
     <BSContentContainer>
-      {giftList.map((gift, index) => (
-        <ConListItem
-          key={index}
-          category={gift.category}
-          brand={gift.brand}
-          name={gift.name}
-          duedate={gift.duedate}
-          isDeleted={gift.isDeleted}
-          onClick={() => handleItemClick(`${gift.brand}`)}
-        />
-      ))}
+      <BSContentList>
+        {giftList.map((gift, index) => (
+          <ConListItem
+            key={index}
+            image_url={gift.image_url}
+            category={gift.category}
+            brand={gift.brand}
+            name={gift.name}
+            duedate={gift.duedate}
+            isDeleted={gift.isDeleted}
+            onClick={() => handleItemClick(`${gift.brand}`)}
+          />
+        ))}
+      </BSContentList>
 
-      <div style={{ padding: '20px 0' }}>
+      <div style={{ padding: '1.25rem 0', textAlign: 'center' }}>
         <p>더 이상 기프티콘이 없어요!</p>
       </div>
     </BSContentContainer>
