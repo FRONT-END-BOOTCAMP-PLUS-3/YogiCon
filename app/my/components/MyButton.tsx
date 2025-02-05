f1b901ca9b395206906cea4534d28bd0719f2876
Taemin1 <rlaxoals132@gmail.com>
2025-02-05 14:56:57 +0900

    refactor: onclick 이벤트 myButton 처리에서 page처리로 변경

--- app/my/components/MyButton.tsx
+++ app/my/components/MyButton.tsx
@@ -1,12 +1,11 @@
-import { useRouter } from 'next/navigation';
+// MyButton.tsx
 import { CiTrash } from 'react-icons/ci';
 import { IoIosLogOut } from 'react-icons/io';
 import styled from 'styled-components';
-import ModalDialog from '@/components/ModalDialog';
-import { useState } from 'react';
 
 interface MyButtonProps {
   id: 'trash' | 'logout' | 'deleteID';
+  onClick: () => void;
 }
 
 /* ---------------------------------- style --------------------------------- */
@@ -42,52 +41,24 @@ const StyledLogo = styled(IoIosLogOut)`
   margin-right: 1.6rem;
 `;
 
-const ModalContent = styled.div`
-  margin-bottom: 1rem;
-`;
-
 /* ---------------------------------- component --------------------------------- */
-const MyButton = ({ id }: MyButtonProps) => {
-  const router = useRouter();
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
-
-  const handleConfirm = () => {
-    if (id === 'logout') {
-      console.log('로그아웃 실행');
-      router.push('/login');
-    } else if (id === 'deleteID') {
-      console.log('회원탈퇴 실행');
-      router.push('/');
-    }
-    closeModal();
-  };
-
+const MyButton = ({ id, onClick }: MyButtonProps) => {
   const ButtonConfig = {
     trash: {
       title: '휴지통',
       icon: <StyledTrashCan />,
-      onClick: () => router.push('/trash'),
     },
     logout: {
       title: '로그아웃',
       icon: <StyledLogo />,
-      onClick: () => {
-        openModal();
-      },
     },
     deleteID: {
       title: '회원탈퇴',
       icon: null,
-      onClick: () => {
-        openModal();
-      },
     },
   };
 
-  const { title, icon, onClick } = ButtonConfig[id];
+  const { title, icon } = ButtonConfig[id];
 
   return (
     <Container>
@@ -95,20 +66,6 @@ const MyButton = ({ id }: MyButtonProps) => {
         <span>{title}</span>
         {icon}
       </ButtonWrapper>
-      <ModalDialog
-        isOpen={isModalOpen}
-        buttonCount={2}
-        onConfirm={handleConfirm}
-        onClose={closeModal}
-      >
-        <ModalContent>
-          {id === 'logout' ? (
-            <span>정말 로그아웃 하시겠습니까?</span>
-          ) : (
-            <span>정말 탈퇴 하시겠습니까?</span>
-          )}
-        </ModalContent>
-      </ModalDialog>
     </Container>
   );
 };
--- app/my/page.tsx
+++ app/my/page.tsx
@@ -1,10 +1,16 @@
+// My.tsx (페이지 컴포넌트)
 'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import styled from 'styled-components';
 
 import Alarm from '@/app/my/components/Alarm';
 import MyButton from '@/app/my/components/MyButton';
 import Name from '@/app/my/components/Name';
-import styled from 'styled-components';
+import ModalDialog from '@/components/ModalDialog';
 
+/* ---------------------------------- style --------------------------------- */
 const MyContainer = styled.div`
   background-color: var(--lightgray);
   height: calc(100vh - 1.5rem);
@@ -13,14 +19,62 @@ const MyContainer = styled.div`
   gap: 1.5rem;
 `;
 
+const ModalContent = styled.div`
+  margin-bottom: 1rem;
+`;
+
+/* ---------------------------------- component --------------------------------- */
 const My = () => {
+  const router = useRouter();
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const [actionType, setActionType] = useState<'logout' | 'deleteID' | null>(
+    null
+  );
+
+  const openModal = (type: 'logout' | 'deleteID') => {
+    setActionType(type);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setActionType(null);
+  };
+
+  const handleConfirm = () => {
+    if (actionType === 'logout') {
+      console.log('로그아웃 실행');
+      router.push('/login');
+    } else if (actionType === 'deleteID') {
+      console.log('회원탈퇴 실행');
+      router.push('/');
+    }
+    closeModal();
+  };
+
   return (
     <MyContainer>
       <Name />
       <Alarm />
-      <MyButton id="trash" />
-      <MyButton id="logout" />
-      <MyButton id="deleteID" />
+      <MyButton id="trash" onClick={() => router.push('/trash')} />
+      <MyButton id="logout" onClick={() => openModal('logout')} />
+      <MyButton id="deleteID" onClick={() => openModal('deleteID')} />
+
+      <ModalDialog
+        isOpen={modalOpen}
+        buttonCount={2}
+        onConfirm={handleConfirm}
+        onClose={closeModal}
+      >
+        <ModalContent>
+          {actionType === 'logout' ? (
+            <span>정말 로그아웃 하시겠습니까?</span>
+          ) : (
+            <span>정말 탈퇴 하시겠습니까?</span>
+          )}
+        </ModalContent>
+      </ModalDialog>
     </MyContainer>
   );
 };
