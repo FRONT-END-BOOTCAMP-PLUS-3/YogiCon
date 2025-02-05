b25a303013d857940e22ce47cd56cb1b1d819c0b
JoowonJang <wndnjs5248@hanmail.net>
2025-02-05 13:49:00 +0900

    feat: trash UI 완성

---
+++ app/trash/page.tsx
@@ -0,0 +1,88 @@
+'use client';
+
+import ConListItem from '@/components/ConListItem';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { ConInfo } from '../giftData';
+
+const TrashContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: calc(100vh - 3.375rem);
+`;
+
+const ConList = styled.ul`
+  width: 100%;
+  padding: 0 5% 5.25rem 5%;
+`;
+
+const NoConImg = styled(Image)`
+  width: 52%;
+  height: auto;
+  aspect-ratio: 205/268;
+  margin-bottom: 1.5rem;
+`;
+const NoConText = styled.p`
+  white-space: pre-line;
+  color: var(--disabled);
+  text-align: center;
+  font-size: 1.5rem;
+  font-weight: bold;
+  line-height: 44px; /* 183.333% */
+  letter-spacing: -0.32px;
+`;
+
+const Trash = () => {
+  const [trashList, setTrashList] = useState<ConInfo[] | null>(null);
+  console.log(trashList);
+
+  useEffect(() => {
+    const fetchTrashList = async () => {
+      try {
+        const res = await fetch(`/api/trash`);
+
+        if (!res.ok) {
+          throw new Error('Response Error');
+        }
+
+        const data = await res.json();
+        setTrashList(data);
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          console.error(err.message);
+        } else {
+          console.error('An unexpected error occurred');
+        }
+      }
+    };
+
+    fetchTrashList();
+  }, []);
+
+  return (
+    <TrashContainer>
+      {trashList?.length ? (
+        <ConList>
+          {trashList.map((item) => (
+            <ConListItem key={item.id} {...item} />
+          ))}
+        </ConList>
+      ) : (
+        <>
+          <NoConImg
+            src="/no_gift_image.webp"
+            priority={true}
+            width={205}
+            height={268}
+            alt="선물상자 캐릭터"
+          />
+          <NoConText>{'삭제된 기프티콘이 없어요!'}</NoConText>
+        </>
+      )}
+    </TrashContainer>
+  );
+};
+
+export default Trash;
