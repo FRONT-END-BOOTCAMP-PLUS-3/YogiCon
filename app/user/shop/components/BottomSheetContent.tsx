16badfeea43ab77c8a6bac65543de823b1b38b9d
yezzero <yeyeonggim06@gmail.com>
2025-02-10 11:25:05 +0900

    modify: giftList 더미데이터가 아닌 api로 가져오도록 변경

--- app/user/shop/components/BottomSheetContent.tsx
+++ app/user/shop/components/BottomSheetContent.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 
-import { giftList } from '@/app/giftData';
+import { GiftDto } from '@/application/usecases/gift/dto/GiftDto';
 import GiftListItem from '@/components/GiftListItem';
 import EventBus from '@/types/EventBus';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 /* ---------------------------------- style --------------------------------- */
@@ -32,26 +34,59 @@ export default function BottomSheetContent({
   moveSheetToBottom,
 }: BottomSheetContentProps) {
   const handleItemClick = (key: string) => {
-    const newKey = new String(key).toString();
-    setSelectedItemKey(newKey);
+    setSelectedItemKey(key);
     EventBus.emit('itemClicked', true);
     moveSheetToBottom();
   };
+  const [giftList, setGiftList] = useState<GiftDto[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const getGifts = async (): Promise<any> => {
+      try {
+        setLoading(true);
+        // disabled 되지 않은 gift list 가져오는 api 사용
+        const res = await fetch('/api/user/shop', { method: 'GET' });
+        const result = await res.json();
+
+        if (!result.success) {
+          console.error('API 오류:', result.message);
+          setGiftList([]);
+          return [];
+        }
+        console.log('가져옴', result.data);
+        setGiftList(result.data);
+      } catch (error) {
+        console.error('데이터 로드 실패:', error);
+        return [];
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getGifts();
+  }, []);
 
   return (
     <BSContentContainer>
-      <BSContentList>
-        {giftList.map((gift, index) => (
-          <GiftListItem
-            key={index}
-            {...gift}
-            onClick={() => handleItemClick(`${gift.brand}`)}
-          />
-        ))}
-      </BSContentList>
+      {giftList && (
+        <BSContentList>
+          {giftList.map((gift: GiftDto, index) => (
+            <GiftListItem
+              key={index}
+              {...gift}
+              onClick={() => handleItemClick(`${gift.brand}`)}
+            />
+          ))}
+        </BSContentList>
+      )}
 
       <div style={{ padding: '1.25rem 0', textAlign: 'center' }}>
-        <p>더 이상 기프티콘이 없어요!</p>
+        {loading ? (
+          <p>기프티콘을 불러오는 중입니다 ...</p>
+        ) : (
+          <p>더 이상 기프티콘이 없어요!</p>
+        )}
       </div>
     </BSContentContainer>
   );
