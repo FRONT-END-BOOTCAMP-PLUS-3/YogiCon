e09fcd920267b38adf3abb0cc14fa0d6e604afc5
Taemin1 <rlaxoals132@gmail.com>
2025-02-10 15:36:41 +0900

    feat: 카카오 로그인 1차구현

--- app/(anon)/page.tsx
+++ app/(anon)/page.tsx
@@ -1,5 +1,27 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  cursor: pointer;
+  img {
+    width: 60%;
+  }
+`;
+
 const Page = () => {
-  return <div></div>;
+  const handleLogin = () => {
+    window.location.href = '/api/auth/login';
+  };
+  return (
+    <Container onClick={handleLogin}>
+      <img src="kakao_login_large_narrow.png"></img>
+    </Container>
+  );
 };
 
 export default Page;
