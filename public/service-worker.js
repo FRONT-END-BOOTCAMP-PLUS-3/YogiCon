938551dcae148e013a29f4966b6969374d049347
JoowonJang <wndnjs5248@hanmail.net>
2025-02-11 11:52:11 +0900

    feat: service worker  생성

---
+++ public/service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener('push', (event) => {
+  const data = JSON.parse(event.data.text());
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/icon512_maskable.png',
+    })
+  );
+});
+
+self.addEventListener('notificationclick', (event) => {
+  clients.openWindow('/'); // 링크 처리해줘야함
+});
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
