0bc52341d80056aeab6dc401ba593b88f5ece116
Taemin1 <rlaxoals132@gmail.com>
2025-02-12 16:52:39 +0900

    modify: useState 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { GlobalStyles } from './globalStyles';
 import { useRouter } from 'next/navigation';
 
