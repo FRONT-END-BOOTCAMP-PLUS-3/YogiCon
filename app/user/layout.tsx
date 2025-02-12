590cf63d581c1a77087abe1e66831b10a56d6c40
Taemin1 <rlaxoals132@gmail.com>
2025-02-12 16:50:51 +0900

    modify: 필요없는 코드 삭제

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -3,16 +3,14 @@
 import Header from '@/components/Header/Header';
 import Navbar from '@/components/Navbar';
 import { usePathname, useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
   const router = useRouter();
-  const [accessToken, setAccessToken] = useState<string | null>(null);
 
   useEffect(() => {
     const token = localStorage.getItem('access_token');
-    setAccessToken(token);
 
     if (!token) {
       router.push('/');
